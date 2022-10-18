b549669fbac4864a40da928dd9b231f8e724c588
Marc Loyat <marc@danka.fr>
2022-10-18 20:39:07 +0200

    Filtres pour le programme

---
+++ web/js/main.js
@@ -0,0 +1,14 @@
+
+
+['date','commune'].forEach((filterName)=>{
+  const filtre = document.getElementById(`filtre-${filterName}`);
+  const els = document.querySelectorAll(`.carte[data-${filterName}]`);
+  const onChange = ()=>{
+    const val = filtre.value;
+    els.forEach((el) => {
+      const visible = val === "all"  || el.dataset[filterName].indexOf(val) != -1;
+      el.style.display = visible ? "block" : "none";
+    });
+  }
+  filtre.addEventListener("change", onChange);
+});
